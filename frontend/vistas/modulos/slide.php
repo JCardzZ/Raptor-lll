c89ba404f358b5aec6e72bc16dd0c00c3d7a34a3
JCardzZ <cardozamanuel96@gmail.com>
2020-04-05 18:54:15 -0600

    Frontend

---
+++ frontend/vistas/modulos/slide.php
@@ -0,0 +1,83 @@
+<!--=====================================
+		DESLIZADOR DE  DIAPOSITIVAS
+======================================-->
+
+<div class="container-fluid" id="slide">
+	
+	<div class="row">
+		
+		
+
+		<ul>
+
+			<!-- DIAPOSITIVA 1 -->
+		
+			<li>
+				
+				<img src="http://localhost:81/backend/vistas/img/slide/default/back_default.jpg" >
+
+				<div class="slideOpciones slideOpcion1">
+					
+					<img class="imgProducto" src="http://localhost:81/backend/vistas/img/slide/slide1/calzado_original.jpeg" >
+					<div class="textosSlide">
+						
+						<h1>Descripcion</h1>
+
+						<h2>PRECIO</h2>
+
+						<h3>CANTIDAD</h3>
+
+						<a href="#">
+							
+							<button class="btn btn-success  text-uppercase">
+
+							VER PRODUCTO <span class="fa fa-chevron-right"></span>
+
+							</button>
+
+						</a>
+
+					</div>	
+
+				</div>
+
+			</li>	
+
+
+		<ul>
+
+			<!-- DIAPOSITIVA 2 
+		
+			<li>
+				
+				<img src="http://localhost:81/backend/vistas/img/slide/default/back_default.jpg">
+
+				<div class="slideOpciones slideOpcion2">
+					
+						<img class="imgProducto" src="http://localhost:81/backend/vistas/img/slide/slide2/curso.png" >
+
+
+					<div class="textosSlide" style="top:20%; left:10%; width:40%">
+						
+						<h1 style="color:#333">Descripcion</h1>
+
+						<h2 style="color:#777">PRECIO</h2>
+
+						<h3 style="color:#888">CANTIDAD</h3>
+
+						<a href="#">
+							
+							<button class="btn btn-success  text-uppercase">
+
+							VER PRODUCTO <span class="fa fa-chevron-right"></span>
+
+							</button>
+
+						</a>
+
+					</div>	
+
+				</div>
+
+			</li>	
+			-->
